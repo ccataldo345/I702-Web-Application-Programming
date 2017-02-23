b60f0963a55c8a37da364a9726b2a4f458afb470
Christian Cataldo <christian.cataldo@itcollege.ee>
2017-02-23 17:53:05 +0200

    Fix cart.php

--- cart.php
+++ cart.php
@@ -2,11 +2,19 @@
 
 <?php
 #require_once "config.php";
+<<<<<<< HEAD
 include ("header.php");
 
 	#$conn = new mysqli("localhost", "test", "t3st3r123", "test");
 	#$results = $conn->query("SELECT * FROM ccataldo_shop_product;");
 
+=======
+#include "header.php";
+
+	$conn = new mysqli("localhost", "test", "t3st3r123", "test");
+	$results = $conn->query("SELECT * FROM ccataldo_shop_product;");
+		
+>>>>>>> 01f86022776cac12a748b89ab57fab4e3f116cfc
 	$product_id = intval($_POST["id"]);
 	if (array_key_exists($product_id, $_SESSION["cart"])) {
 		$_SESSION["cart"][$product_id] += 1;
@@ -15,8 +23,16 @@ include ("header.php");
 	}
 	var_dump($_SESSION["cart"]);
 
+<<<<<<< HEAD
 #$conn->close();
 ?>
 <br />
 <br />
 <a href="lab01.php">Back to product listing</a>
+=======
+$conn->close();
+?>
+<br />
+<br />
+<a href="lab01.php">Back to product listing</a>
+>>>>>>> 01f86022776cac12a748b89ab57fab4e3f116cfc
--- header.php
+++ header.php
@@ -1,7 +1,25 @@
+<<<<<<< HEAD
+=======
+<?php
+#require_once "config.php";
+include "header.php";
+$conn = new mysqli("localhost", "test", "t3st3r123", "test");
+if ($conn->connect_error)
+  die("Connection to database failed:" .
+    $conn->connect_error);
+$conn->query("set names utf8"); // Support umlaut characters
+?>
+<!-- Page specific stuff goes here -->
+<? include "footer.php" ?>
+>>>>>>> 01f86022776cac12a748b89ab57fab4e3f116cfc
 
 <?php
 session_start();
 if (!array_key_exists("cart", $_SESSION)) {
     $_SESSION["cart"] = array();
 }
+<<<<<<< HEAD
 ?>
+=======
+?>
+>>>>>>> 01f86022776cac12a748b89ab57fab4e3f116cfc
